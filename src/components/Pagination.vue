508e40dfc4e053fcabd7085ae77841e06619531a
Azeem Akhtar <azeemakhtar@Azeem-Akhtar.local>
2024-08-08 09:24:13 +0500

    pagination commit

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -24,7 +24,7 @@
         </v-col>
 
 
-
+<!-- ======================================================  OFFSET INCREMENT  ======================================================================== -->
     <!-- If selected Strategy Option is Offset Increment -->
     <v-col cols="12" v-if="paginationStrategy === 'Offset Increment'">
             <div>
@@ -85,70 +85,358 @@
                     </div>
 
                 </div>
-        
+
+              <!-- Show Inject Offset into Outgoing HTTP request  -->
+                  <v-col cols="12">
+                    <v-checkbox
+                      v-model="injectOffsetHttp"
+                      label="Inject Offset into outgoing HTTP Request"
+                      @change="onInjectOffsetHttpToggle"
+                    />
+                  </v-col>
+
+                  <v-col cols="12" v-if="injectOffsetHttp">
+                    <v-select
+                      v-model="injectOffsetIntoHttp"
+                      :items="injectOffsetIntoHttpOptions"
+                      label="Inject Into"
+                      @change="onInjectIntoChange"
+                    />
+
+                  <!-- If selected Inject into option is 'Header' so show this fields  -->
+                    <div v-if="injectOffsetIntoHttp === 'Header'">
+                      <v-text-field
+                      v-model="headerName"
+                      label="Header Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                  <div v-if="injectOffsetIntoHttp === 'Query Parameter'">
+                      <v-text-field
+                      v-model="parameterName"
+                      label="Parameter Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                  <div v-if="injectOffsetIntoHttp === 'Body Data (urlencoded form)'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
+                  <div v-if="injectOffsetIntoHttp === 'Body JSON payload'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                </v-col>
 
 
+              <!-- Toggle Option for the Inject on First Request -->
+                  <v-col cols="12">
+                    <v-switch
+                      v-model="injectOnFirstRequest"
+                      label="Inject on first request"
+                    />
+                  </v-col>
+            </v-col>
 
-    <!-- Show Inject Offset into Outgoing HTTP request  -->
-        <v-col cols="12">
-          <v-checkbox
-            v-model="injectOffsetHttp"
-            label="Inject Offset into outgoing HTTP Request"
-            @change="onInjectOffsetHttpToggle"
-          />
-        </v-col>
 
-        <v-col cols="12" v-if="injectOffsetHttp">
-          <v-select
-            v-model="injectOffsetIntoHttp"
-            :items="injectOffsetIntoHttpOptions"
-            label="Inject Into"
-            @change="onInjectIntoChange"
-          />
+<!-- ======================================================  PAGE INCREMENT  ======================================================================== -->
+    <!-- If selected Strategy Option is Page Increment -->
+    <v-col cols="12" v-if="paginationStrategy === 'Page Increment'">
+            <div>
+            <p>Page Size<span style="color: grey; font-size: 10px; font-style: italic;">Optional</span></p>
+                <v-text-field
+                    v-model="pageSize"
+                    type="number"
+                    :error-messages="limitPageSizeMessages"
+                    @input="validatePageSize"
+                />
 
-        <!-- If selected Inject into option is 'Header' so show this fields  -->
-          <div v-if="injectOffsetIntoHttp === 'Header'">
-            <v-text-field
-            v-model="headerName"
-            label="Header Name"
-          />
+            <p>Start From Page<span style="color: grey; font-size: 10px; font-style: italic;">Optional</span></p>
+                <v-text-field
+                    v-model="startFromPage"
+                    type="number"
+                    :error-messages="limitPageSizeMessages"
+                    @input="validatePageSize"
+                />
+            </div>
+            
+        <!-- Show Inject Page Size into HTTP outgoing request (if pageSize>0)  -->
+            <v-checkbox
+                v-if="pageSize > 0"
+                    v-model="injectPageSizetHttp"
+                    label="Inject Limit into outgoing HTTP Request"
+                />
+
+                <div v-if="injectPageSizetHttp">
+                    <v-select
+                        v-model="injectPageSizeIntoHttp"
+                        :items="injectPageSizeIntoHttpOptions"
+                        label="Inject Into"
+                        @change="onInjectIntoChange"
+                    />
+
+                    <!-- If selected Inject into option is 'Header' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Header'">
+                        <v-text-field
+                        v-model="pageSizeHeaderName"
+                        label="Header Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Query Parameter'">
+                        <v-text-field
+                        v-model="pageSizeParameterName"
+                        label="Parameter Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Body Data (urlencoded form)'">
+                        <v-text-field
+                        v-model="pageSizeKeyName"
+                        label="Key Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Body JSON payload'">
+                        <v-text-field
+                        v-model="pageSizeKeyName"
+                        label="Key Name"
+                        />
+                    </div>
+
+                </div>
+
+              <!-- Show Inject Page Number into Outgoing HTTP request  -->
+                  <v-col cols="12">
+                    <v-checkbox
+                      v-model="injectPageNumberHttp"
+                      label="Inject Page Number into outgoing HTTP Request"
+                      @change="onInjectPageNumberHttpToggle"
+                    />
+                  </v-col>
+
+                  <v-col cols="12" v-if="injectPageNumberHttp">
+                    <v-select
+                      v-model="injectPageNumberIntoHttp"
+                      :items="injectPageNumberIntoHttpOptions"
+                      label="Inject Into"
+                      @change="onInjectIntoChange"
+                    />
+
+                  <!-- If selected Inject into option is 'Header' so show this fields  -->
+                    <div v-if="injectPageNumberIntoHttp === 'Header'">
+                      <v-text-field
+                      v-model="headerName"
+                      label="Header Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                  <div v-if="injectPageNumberIntoHttp === 'Query Parameter'">
+                      <v-text-field
+                      v-model="parameterName"
+                      label="Parameter Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                  <div v-if="injectPageNumberIntoHttp === 'Body Data (urlencoded form)'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
+                  <div v-if="injectPageNumberIntoHttp === 'Body JSON payload'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                </v-col>
+
+
+              <!-- Toggle Option for the Inject on First Request -->
+                  <v-col cols="12">
+                    <v-switch
+                      v-model="InjectPageIncrementOnFirstRequest"
+                      label="Inject on first request"
+                    />
+                  </v-col>
+            </v-col>
+
+
+<!-- ======================================================  Cursor Pagination  ======================================================================== -->
+<!-- If selected Strategy Option is Cursor Pagination -->
+<v-col cols="12" v-if="paginationStrategy === 'Cursor Pagination'">
+          <div>
+            <p>Next Page Cursor</p>
+              <v-select
+                v-model="nextPageCursor"
+                :items="nextPageCursorOptions"
+                label="Inject Into"
+                @change="onInjectIntoChange"
+              />
+
+                <div v-if="nextPageCursor === 'Response'">
+                  <p>Path</p>
+                        <v-text-field
+                        v-model="cursorPath"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                    <div v-if="nextPageCursor === 'Header'">
+                      <p>Path</p>
+                        <v-text-field
+                        v-model="cursorPath"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                    <div v-if="nextPageCursor === 'Custom'">
+                      <p>Cursor Value</p>
+                        <v-text-field
+                        v-model="cursorValue"
+                        />
+
+                        <p>Stop Condition <span style="color: grey; font-size: 10px; font-style: italic;">Optional</span></p>
+                        <v-text-field
+                        v-model="cursorValue"
+                        />
+                    </div>
+
+
+              <!-- Show Inject Cursor Value into Outgoing HTTP request  -->
+                  <v-col cols="12">
+                    <v-checkbox
+                      v-model="injectCursorValueHttp"
+                      label="Inject Cursor Value into outgoing HTTP Request"
+                      @change="onInjectOffsetHttpToggle"
+                    />
+                  </v-col>
+
+                  <v-col cols="12" v-if="injectCursorValueHttp">
+                    <v-select
+                      v-model="injectCursorvalueIntoHttp"
+                      :items="injectCursorvalueIntoHttpOptions"
+                      label="Inject Into"
+                      @change="onInjectIntoChange"
+                    />
+
+                  <!-- If selected Inject into option is 'Header' so show this fields  -->
+                    <div v-if="injectCursorvalueIntoHttp === 'Header'">
+                      <v-text-field
+                      v-model="headerName"
+                      label="Header Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                  <div v-if="injectCursorvalueIntoHttp === 'Query Parameter'">
+                      <v-text-field
+                      v-model="parameterName"
+                      label="Parameter Name"
+                    />
+                    </div>
+
+                  <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                  <div v-if="injectCursorvalueIntoHttp === 'Body Data (urlencoded form)'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
+                  <div v-if="injectCursorvalueIntoHttp === 'Body JSON payload'">
+                      <v-text-field
+                      v-model="keyName"
+                      label="Key Name"
+                    />
+                    </div>
+
+                </v-col>
+
+                <p>Page Size<span style="color: grey; font-size: 10px; font-style: italic;">Optional</span></p>
+                <v-text-field
+                    v-model="pageSize"
+                    type="number"
+                    :error-messages="limitPageSizeMessages"
+                    @input="validatePageSize"
+                />
+
+              <v-checkbox
+                v-if="pageSize > 0"
+                    v-model="injectPageSizetHttp"
+                    label="Inject Page Size into outgoing HTTP Request"
+                />
+
+                <div v-if="injectPageSizetHttp">
+                    <v-select
+                        v-model="injectPageSizeIntoHttp"
+                        :items="injectPageSizeIntoHttpOptions"
+                        label="Inject Into"
+                        @change="onInjectIntoChange"
+                    />
+
+                    <!-- If selected Inject into option is 'Header' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Header'">
+                        <v-text-field
+                        v-model="pageSizeHeaderName"
+                        label="Header Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Query Parameter'">
+                        <v-text-field
+                        v-model="pageSizeParameterName"
+                        label="Parameter Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Body Data (urlencoded form)'">
+                        <v-text-field
+                        v-model="pageSizeKeyName"
+                        label="Key Name"
+                        />
+                    </div>
+
+                    <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
+                    <div v-if="injectPageSizeIntoHttp === 'Body JSON payload'">
+                        <v-text-field
+                        v-model="pageSizeKeyName"
+                        label="Key Name"
+                        />
+                    </div>
+
+                </div>
           </div>
 
-        <!-- If selected Inject into option is 'Query Parameter' so show this fields  -->
-        <div v-if="injectOffsetIntoHttp === 'Query Parameter'">
-            <v-text-field
-            v-model="parameterName"
-            label="Parameter Name"
-          />
-          </div>
-
-        <!-- If selected Inject into option is 'Body Data (urlencoded form)' so show this fields  -->
-        <div v-if="injectOffsetIntoHttp === 'Body Data (urlencoded form)'">
-            <v-text-field
-            v-model="keyName"
-            label="Key Name"
-          />
-          </div>
-
-          <!-- If selected Inject into option is 'Body JSON payload' so show this fields  -->
-        <div v-if="injectOffsetIntoHttp === 'Body JSON payload'">
-            <v-text-field
-            v-model="keyName"
-            label="Key Name"
-          />
-          </div>
-
-        </v-col>
 
 
-    <!-- Toggle Option for the Inject on First Request -->
-        <v-col cols="12">
-          <v-switch
-            v-model="injectOnFirstRequest"
-            label="Inject on first request"
-          />
-        </v-col>
-    </v-col>
+
+</v-col>
+
+
+
+
 
       </v-row>
     </v-container>
@@ -198,6 +486,75 @@
 
         showLimitField: false,
         showParameterNameField: false,
+
+
+
+        // Page Increment vars
+        pageSize: 'null',
+        PageSizeErrorMessages: '',
+
+        startFromPage: 'null',
+
+        injectPageSizetHttp: false,
+        injectPageSizeIntoHttp: 'Query parameter',
+        pageSizeHeaderName: '',
+        pageSizeParameterName: '',
+        pageSizeKeyName: '',
+        InjectPageIncrementOnFirstRequest: false,
+
+        // Inject Page Size into HTTP Options
+        injectPageSizeIntoHttpOptions: [
+          'Query Parameter',
+          'Header',
+          'Body Data (urlencoded form)',
+          'Body JSON payload'
+        ],
+
+        // Inject Page Number into HTTP Options
+        injectPageNumberHttp: true,
+        injectPageNumberIntoHttp: 'Query Parameter',
+        injectPageNumberIntoHttpOptions: [
+          'Query Parameter',
+          'Header',
+          'Path',
+          'Body Data (urlencoded form)',
+          'Body JSON payload'
+        ],
+
+
+
+
+        // Cursor Pagination Vars
+        nextPageCursor: 'Response',
+
+        cursorPath: '',
+        cursorValue: '',
+
+        // Inject Page Size into HTTP Options
+        nextPageCursorOptions: [
+          'Response',
+          'Header',
+          'Custom'
+        ],
+
+
+        // Inject Cursor Value into outgoing HTTP request
+        injectCursorValueHttp: true, 
+        injectCursorvalueIntoHttp: 'Query parameter',
+        injectCursorValueIntoHttpOptions: [
+          'Query Parameter',
+          'Header',
+          'Path',
+          'Body Data (urlencoded form)',
+          'Body JSON payload'
+        ],
+
+
+
+
+
+
+
       };
     },
     methods: {
@@ -235,6 +592,14 @@
         this.limitErrorMessages = 'Limit cannot be negative';
       } else {
         this.limitErrorMessages = '';
+      }
+    },
+
+    validatePageSize() {
+      if (this.pageSize < 0) {
+        this.PageSizeErrorMessages = 'Page Size cannot be negative';
+      } else {
+        this.PageSizeErrorMessages = '';
       }
     },
 
